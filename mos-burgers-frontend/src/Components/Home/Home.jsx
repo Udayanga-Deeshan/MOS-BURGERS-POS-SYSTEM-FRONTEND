1d608c16f0f8a396764958c50edea1072b46c5f0
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2025-03-24 16:14:03 +0530

    home page UI completed

---
+++ mos-burgers-frontend/src/Components/Home/Home.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Home.css";
+
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <div className="land-page-body">
+        <header className="land-page-header">
+          <div className="land-page-logo">MOS BURGERS</div>
+          <nav>
+            <ul className="land-page-nav-links">
+              <li>
+                <p onClick={() => navigate("/home")}>Home</p>
+              </li>
+
+              <li>
+                <p onClick={() => navigate("/customer")}>Manage Customers</p>
+              </li>
+              <li>
+                <p onClick={() => navigate("/stock")}>Inventory</p>
+              </li>
+              
+              
+
+              <li>
+                <p onClick={() => navigate("/placeOrder")}>Order Now</p>
+              </li>
+            </ul>
+          </nav>
+          <button
+            className="land-page-btn-cashier"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Logout
+          </button>
+        </header>
+
+        <section className="land-page-hero">
+          <div className="land-page-hero-content">
+            <h1 className="land-page-hero-text">BIG TASTE. ZERO COMPROMISE</h1>
+            <p className="land-page-hero-subtext">
+              Taste the difference. Fresh, handcrafted, and made just for you.
+            </p>
+
+            
+
+
+
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
